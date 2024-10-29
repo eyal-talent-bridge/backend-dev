6172a147f424efce11c643860d62893de4d9af26
eyalyehiely <eyalwork0@gmail.com>
2024-10-29 15:05:16 +0700

    remove unuse code

--- users/utils.py
+++ users/utils.py
@@ -1,11 +1,7 @@
-import re,logging,os,requests
-from rest_framework.response import Response
+import logging,os,requests
 from .models import *
-from urllib.parse import urlparse
 from PyPDF2 import PdfReader
 from backend.settings import NOTIFICATION_SERVICE_URL
-
-
 
 users_logger = logging.getLogger('users')
 # -------------------------------------Talents-----------------------------------------------------------------------------------------------------------------------------------------------
@@ -52,73 +48,6 @@ def scan_cv_for_job_requirements(cv_file, job_requirements):
         users_logger.error(f"Error analyzing CV: {cv_file.name}. Error: {e}", exc_info=True)
         return 0
 
-
-# ---------------------------------Recruiter---------------------------------------------------------------------------------------
-def validate_recruiter_email(email, company_id):
-    # Fetch the company associated with the given company_id
-    company = Company.objects.filter(id=company_id).first()
-    
-    # If company doesn't exist, return an error
-    if not company:
-        return Response({'status': 'error', 'message': 'Company not found'}, status=400)
-
-    # Extract domain from company's email and recruiter's email
-    company_domain = company.email.split('@')[1] if company.email else None
-    recruiter_domain = email.split('@')[1]
-    
-    # Check if company has a valid domain and if the domains match
-    if not company_domain or company_domain != recruiter_domain:
-        return Response({'status': 'error', 'message': 'Email domain does not match company domain'}, status=400)
-
-    # Return True if validation is successful
-    return True
-        
-
-    
-    
-
-
-
-# def validate_working_days(self, value):
-#         valid_days = {'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'}
-#         if not set(value).issubset(valid_days):
-#             raise serializers.ValidationError("Working days must be a subset of valid weekdays.")
-#         return value
-
-# def validate_working_hours(self, value):
-#         if not isinstance(value, dict):
-#             raise serializers.ValidationError("Expected a dictionary for working hours.")
-
-#         for day, hours in value.items():
-#             if not isinstance(hours, dict) or 'start' not in hours or 'end' not in hours:
-#                 raise serializers.ValidationError(f"Invalid working hours format for {day}. Expected a dict with 'start' and 'end' times.")
-            
-#             start_time = hours['start']
-#             end_time = hours['end']
-            
-#             if not (isinstance(start_time, str) and isinstance(end_time, str)):
-#                 raise serializers.ValidationError(f"Working hours for {day} should be in string format.")
-            
-#             try:
-#                 start_hour, start_minute = map(int, start_time.split(':'))
-#                 end_hour, end_minute = map(int, end_time.split(':'))
-#             except ValueError:
-#                 raise serializers.ValidationError(f"Invalid time format for {day}. Expected 'HH:MM' format.")
-            
-#             if not (0 <= start_hour < 24 and 0 <= end_hour < 24):
-#                 raise serializers.ValidationError(f"Hour value out of range for {day}.")
-#             if not (0 <= start_minute < 60 and 0 <= end_minute < 60):
-#                 raise serializers.ValidationError(f"Minute value out of range for {day}.")
-#             if (start_hour, start_minute) >= (end_hour, end_minute):
-#                 raise serializers.ValidationError(f"End time must be after start time for {day}.")
-        
-#         return value
-
-
-def validate_phone_number(phone_number):
-    if not phone_number.isdigit() or len(phone_number) < 9 or len(phone_number) > 15 :
-         Response({'status': 'error', 'message':"Enter a valid phone number."},False)
-    return True
 
 
 
